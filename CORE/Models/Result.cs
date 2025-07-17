afc3478216a9bd41dc09aa9c105ee5dccded3f79
Cagil Alsac <cagilalsac@outlook.com>
2025-07-17 19:28:06 +0300

    CORE katmanı tamamlandı.

--- CORE/Models/Result.cs
+++ CORE/Models/Result.cs
@@ -4,11 +4,23 @@
     {
         public bool IsSuccessful { get; }
         public string Message { get; }
+        public int Id { get; }
 
-        public Result(bool isSuccessful, string message)
+        public Result(bool isSuccessful, string message = "", int id = 0)
         {
             IsSuccessful = isSuccessful;
             Message = message;
+            Id = id;
+        }
+    }
+
+    public class Result<TData> : Result where TData : class, new()
+    {
+        public TData Data { get; }
+
+        public Result(bool isSuccessful, TData data, string message = "") : base(isSuccessful, message)
+        {
+            Data = data;
         }
     }
 }
---
+++ CORE/Services/Service.cs
@@ -0,0 +1,49 @@
+﻿using CORE.Entities;
+using CORE.Models;
+using CORE.Repositoires;
+
+namespace CORE.Services
+{
+    public abstract class Service<TEntity, TRequest, TResponse> : IDisposable 
+        where TEntity : Entity, new() where TRequest : Request, new() where TResponse : Response, new()
+    {
+        protected readonly Repo<TEntity> _repo;
+
+        protected Service(Repo<TEntity> repo)
+        {
+            _repo = repo;
+        }
+
+        public abstract Result<List<TResponse>> GetList();
+        public abstract Result<TResponse> GetItem(int id);
+        public abstract Result Create(TRequest request);
+        public abstract Result Update(TRequest request);
+        public abstract Result Delete(int id);
+
+        protected Result Success(string message = "", int id = 0)
+        {
+            return new Result(true, message, id);
+        }
+
+        protected Result Error(string message = "", int id = 0)
+        {
+            return new Result(false, message, id);
+        }
+
+        protected Result<TData> Success<TData>(TData data, string message = "") where TData : class, new()
+        {
+            return new Result<TData>(true, data, message);
+        }
+
+        protected Result<TData> Error<TData>(TData data, string message = "") where TData : class, new()
+        {
+            return new Result<TData>(false, data, message);
+        }
+
+        public void Dispose()
+        {
+            _repo.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
